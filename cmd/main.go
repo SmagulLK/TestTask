8b3de6273cffff90120bf23641206a97be3daab1
Smagul.Alkey <roket.flesh@gmail.com>
2023-08-04 18:53:51 +0600

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -12,16 +12,13 @@ import (
 )
 
 func main() {
-	if err := InitConfig(); err != nil {
-		log.Fatalf("Error of reading config file: %s \n", err.Error())
-	}
 
 	db, err := repository.NewDB(repository.Config{
-		Host:     os.Getenv("db.host"),
-		Port:     os.Getenv("db.port"),
-		Sslmode:  os.Getenv("db.sslmode"),
-		Username: os.Getenv("db.username"),
-		DbName:   os.Getenv("db.dbname"),
+		Host:     os.Getenv("db_host"),
+		Port:     os.Getenv("db_port"),
+		Sslmode:  os.Getenv("db_sslmode"),
+		Username: os.Getenv("db_username"),
+		DbName:   os.Getenv("db_dbname"),
 		Password: os.Getenv("DB_PASSWORD"),
 	})
 	if err != nil {
@@ -36,9 +33,5 @@ func main() {
 	if err := serv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		panic(err)
 	}
-}
-func InitConfig() error {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
+
 }
